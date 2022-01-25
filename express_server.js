3cb2ea5f2d30d144e3c40dc4c7abe813a63be6fb
mhakbar <habibakbar@live.com>
2022-01-24 19:58:26 -0800

    shortURL and longURL added and displays correctly

--- express_server.js
+++ express_server.js
@@ -4,13 +4,28 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");//This tells the Express app to use EJS as its templating engine.
 
+/////////////////////////////////////URL Database//////////////////////////////////////////
+
+/////////showing long and short URLs//////////
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//////////////////////////////////////////////
 
+/////////passing urlDatabase to express_server.js template/////
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
-
+/*Note: use http://localhost:8080/urls to view URLs. This code is used for handling the route. key is "urls".*/
+/////////////Second route///////////////////
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req.params.longURL);
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]/* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
 ////////////////////////////////////////MAIN PAGE///////////////////////////////////////////
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -37,10 +52,22 @@ app.get("/urls.json", (req, res) => {
 the entire urlDatabase object*/
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-/////////////////////////////////Hello World///////////////////////////////////////////////
+/////////////////////////////////Hello World test code///////////////////////////////////////////////
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+
+
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 
 /*Note: visit http://localhost:8080/hello to see the result */
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 

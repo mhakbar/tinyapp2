35bf1de773a67796d90a778308af535719a14b9d
mhakbar <habibakbar@live.com>
2022-01-28 17:03:45 -0800

    created helper and helperTest file

---
+++ helper.js
@@ -0,0 +1,18 @@
+const getUserByEmail = (email, users) => {
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+}
+
+
+
+
+
+
+
+
+module.exports = getUserByEmail;
